e965ea4ed024052469362daa3361608c39ea29b7
Ali Sawari <ali.dragon.swr@gmail.com>
2022-12-07 00:14:24 +0300

    feat(): add remarks to the generated link

--- ss-genuri.py
+++ ss-genuri.py
@@ -6,12 +6,14 @@ import base64
 parser = argparse.ArgumentParser(description='A script that parses Shadowsocks server configuration file and generates '
                                              'a client configuration URI')
 parser.add_argument('--config', '-c', type=str, help='A path to your Shadowsocks server config.json')
+
 args = parser.parse_args()
+
 if args.config is None:
     args.config='/etc/shadowsocks-libev/config.json'
     print('Using default config path: {}'.format(args.config))
 
-config = json.load(open(args.config))
+config = json.load(open(args.config, 'r'))
 
 uri = "%s:%s@%s:%d" % (
     config['method'],
@@ -20,5 +22,12 @@ uri = "%s:%s@%s:%d" % (
     config['server_port'],
 )
 
-encoded_uri = base64.b64encode(uri.encode('utf-8'))
-print('ss://%s' % encoded_uri.decode('utf-8'))
+encoded_uri = base64.b64encode(uri.encode('utf-8')).decode('utf-8')
+
+remarks = 'Shadowsocks Server'
+
+if 'remarks' in config: remarks = config['remarks']
+
+remarks = "".join(remarks.split(' '))
+
+print(f"ss://{encoded_uri}#{remarks}")
